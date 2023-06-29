9f4fb5d32edfb20bb247d387d8f8e0341080526d
Manan Chataut <100475418+heyits-manan@users.noreply.github.com>
2023-06-29 16:33:59 +0545

    Add Linked List files
    Implementation of Linked List added!

---
+++ DSA/Linked_List/Implementation/Main.java
@@ -0,0 +1,15 @@
+package Implementation;
+
+public class Main {
+    public static void main(String[] args) {
+        var list = new LL();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+
+        // list.removeFirst();
+        // list.removeLast();
+        // System.out.println(list.indexOf(30));
+        // System.out.println(list.contains(50));
+    }
+}
